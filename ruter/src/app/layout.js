49d0eed586c391101b2ab7660ad9880a43d58b1d
Descaplis <92810856+Descaplis@users.noreply.github.com>
2025-10-16 11:15:02 +0200

    Update

--- ruter/src/app/layout.js
+++ ruter/src/app/layout.js
@@ -1,26 +1,42 @@
+"use client"
 import Link from "next/link";
 import "./globals.css";
-
-export const metadata = {
-  title: "RUTer site",
-};
+import Image from "next/image";
+import { usePathname } from "next/navigation";
 
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body className="min-h-screen bg-gray-50 text-gray-800">
-          <h1 className="text-xl font-bold">🌐 RUTer site</h1>
-          <nav className="space-x-4">
-            <Link href="/" className="text-blue-600 hover:underline">
-              Home
-            </Link>
-            <Link href="/about" className="text-blue-600 hover:underline">
-              About
-            </Link>
-            <Link href="/posts" className="text-blue-600 hover:underline">
-              Posts
-            </Link>
-          </nav>
+      <body className="bg-[#e6e6e6]">
+        <div className="p-1 flex space-x-10 text-3xl">
+            <Image 
+            src="/ruter logo.png"
+            width="280"
+            height="200"
+            alt="RUTer logo"
+            className="border-2 bg-blue-100 border-blue-950 rounded-full"
+            />
+            <div className="w-full flex flex-col">
+              <div className="w-full h-1/4 flex justify-between space-x-6 my-3">
+                <Link href="/" className="w-1/3 text-center bg-blue-200 p-4 text-blue-600 rounded-md border hover:bg-blue-300 hover:text-blue-700 transition-all duration-300">
+                  Home
+                </Link>
+                <Link href="/about" className="w-1/3 text-center bg-blue-200 p-4 text-blue-600 rounded-md border hover:bg-blue-300 hover:text-blue-700 transition-all duration-300">
+                  About
+                </Link>
+                <Link href="/posts" className="w-1/3 text-center bg-blue-200 p-4 text-blue-600 rounded-md border hover:bg-blue-300 hover:text-blue-700 transition-all duration-300">
+                  Posts
+                </Link>
+              </div>
+              <div className="flex justify-center items-center h-3/4">
+                {usePathname() == "/" ? (
+                  <h1 className="text-8xl w-full text-center">RUTeR</h1>
+                ) : (
+                  <></>
+                )}
+              </div>
+            </div>
+        </div>
         <main className="p-10">{children}</main>
       </body>
     </html>
--- ruter/src/app/posts/page.jsx
+++ ruter/src/app/posts/page.jsx
@@ -1,5 +1,17 @@
+"use client"
+import { useEffect, useState } from "react";
+import { InstagramEmbed, FacebookEmbed } from "react-social-media-embed"
+
 export default function Posts() {
+    const [isClient, setIsClient] = useState(false);
+    useEffect(() => {
+        setIsClient(true)
+    }, [])
+
     return (
-        <h1>Posts</h1>
+        <>
+            <h1>Posts</h1>
+<iframe src="https://www.facebook.com/plugins/post.php?href=https%3A%2F%2Fwww.facebook.com%2FNela9891%2Fposts%2Fpfbid026vnaDq5JfXAKVwpdDWQtmLnmN6oC5Hoetcmd4RhghuXNrexXEzEoU46Rggo6kawSl&show_text=true&width=500" width="500" height="185" scrolling="no" frameBorder="0" allowFullScreen={true} allow="autoplay; clipboard-write; encrypted-media; picture-in-picture; web-share"></iframe>
+        </>
     );
 }
